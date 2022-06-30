c05aca363f865706fa6b47f23569dc844277ecd2
arthurdauplay <arthur.dauplay2022@campus-eni.fr>
2022-06-30 16:45:07 +0200

    Update UserDAOImpl.java

--- src/main/java/fr/eni/enienchere/dal/UserDAOImpl.java
+++ src/main/java/fr/eni/enienchere/dal/UserDAOImpl.java
@@ -13,6 +13,9 @@ public class UserDAOImpl implements UserDAO{
 
     private static final String CREATE="INSERT INTO UTILISATEURS (pseudo, nom, prenom, adresse, cpo, ville, email, telephone, password) VALUES (?,?,?,?,?,?,?,?,?)";
     private static final String UPDATE="UPDATE UTILISATEURS SET PSEUDO=?,nom=?,prenom=?,telephone=?,rue=?,code_postal=?,ville=?,email=?,telephone=?,password=?,";
+
+    private static final String DELETE="DELETE FROM UTILISATEURS WHERE no_utilisateur = ?";
+
     private static final String VERIFY=""; // TODO requete à faire
     // TODO Requete a faire pour les méthodes
 
@@ -69,7 +72,19 @@ public class UserDAOImpl implements UserDAO{
         }
     }
     public void deleteUser(Integer id) throws DALException {
-        // TODO Auto-generated
+        try (Connection conn = ConnectionProvider.getConnection();) {
+            PreparedStatement stmt = conn.prepareStatement(DELETE);
+
+            stmt.setInt(1,id);
+
+            int nbRows = stmt.executeUpdate();
+
+            if(nbRows == 0) {
+                throw new DALException("erreur delete user - id not found" + id);
+            }
+        }catch (SQLException e) {
+            throw new DALException("erreur delete user : "+id, e);
+        }
     }
     public User selectUser(Integer id) throws DALException {
         // TODO Auto-generated
