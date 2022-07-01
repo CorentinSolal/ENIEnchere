a82ccf8d26d98a1ff2d000b2c3289fa51cc06c43
Tsiory144 <tsiory.ranaivoharison2022@campus-eni.fr>
2022-07-01 10:15:32 +0200

    Tsiory 10:14

--- src/main/java/fr/eni/enienchere/dal/RetraitDAOImpl.java
+++ src/main/java/fr/eni/enienchere/dal/RetraitDAOImpl.java
@@ -7,17 +7,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static sun.tools.jconsole.Messages.CREATE;
 
 public class RetraitDAOImpl implements RetraitDAO {
     // TODO FAIRE LA DAO POUR LIER LA BO A LA BASE DE DONNEES
 
     private static final String INSERT = "INSERT INTO RETRAITS (rue, cpo, ville) VALUES (?, ?, ?)";
+    private static final String UPDATE = "UPDATE RETRAITS SET rue=?, cpo=?, ville=?";
+
+    private static final String DELETE = "DELETE FROM RETRAITS WHERE no_article = ?";
 
     public void addRetrait(Retrait retrait) throws DALException {
         try (Connection conn = ConnectionProvider.getConnection();) {
 
-            PreparedStatement stmt = conn.prepareStatement(CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = conn.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
 
             stmt.setString(1, retrait.getRue());
             stmt.setString(2, retrait.getCpo());
@@ -34,6 +36,39 @@ public class RetraitDAOImpl implements RetraitDAO {
         }
     }
 
+    public void updateRetrait (Retrait retrait) throws DALException {
+        try (Connection conn = ConnectionProvider.getConnection();) {
+            PreparedStatement stmt = conn.prepareStatement(UPDATE);
+
+            stmt.setString(1, retrait.getRue());
+            stmt.setString(2, retrait.getCpo());
+            stmt.setString(3, retrait.getVille());
+
+            int nbRows = stmt.executeUpdate();
+
+            if(nbRows == 0) {
+                throw new DALException("retrait not found");
+            }
+        } catch (SQLException e) {
+            throw new DALException("error update retrait : ", e);
+        }
+    }
+
+    public void deleteRetrait (int noArticle) throws DALException {
+        try (Connection conn = ConnectionProvider.getConnection();) {
+            PreparedStatement stmt = conn.prepareStatement(DELETE);
+
+            stmt.setInt(1, noArticle);
+
+            int nbRows = stmt.executeUpdate();
+
+            if(nbRows == 0) {
+                throw new DALException("retrait not found");
+            }
+        } catch (SQLException e) {
+            throw new DALException("error delete retrait : ", e);
+        }
+    }
 
     //TODO REPRENDRE LES MEMES METHODES QUE LES AUTRES DAO
 
