3bef9054f7b61c9d8936b09e5d2c5eaa34ff9d7c
CorentinSolal <corentin.solal2022@campus-eni.fr>
2022-07-05 11:16:15 +0200

    Co 11h15

--- src/main/java/fr/eni/enienchere/ihm/ProfilServlet.java
+++ src/main/java/fr/eni/enienchere/ihm/ProfilServlet.java
@@ -1,5 +1,10 @@
 package fr.eni.enienchere.ihm;
 
+import fr.eni.enienchere.bll.BLLException;
+import fr.eni.enienchere.bll.BLLFactory;
+import fr.eni.enienchere.bll.UserManager;
+import fr.eni.enienchere.bo.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -16,7 +21,15 @@ public class ProfilServlet extends HttpServlet {
     private HttpSession session;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserManager um= BLLFactory.getUserManager();
+        User user= null;
+        try {
+            user = um.selectUser(12);
+        } catch (BLLException e) {
+            throw new RuntimeException(e);
+        }
         session = request.getSession();
+        request.setAttribute("user",user);
         getServletContext().getRequestDispatcher(PROFIL).forward(request, response);
     }
 
